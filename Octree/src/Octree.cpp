cd2f33c56e706a351b1ec41ed01b1233ff578fa0
Edddd06 <eduard2sorio@gmail.com>
2026-09-23 21:38:48 -0500

    Agregando las ultimas funciones

--- Octree/src/Octree.cpp
+++ Octree/src/Octree.cpp
@@ -11,15 +11,6 @@ Octree::~Octree(){
     liberar(root) ;  // usamos el liberar que era el extra de los extras xd
 }
 
-void Octree::liberar(OctreeNode* nodo ) {
-    if(nodo==nullptr ){
-        return; 
-    }
-    for(int i = 0; i <8 ;i++ ) {
-        liberar(nodo->hijos[i]); 
-    }
-    delete nodo ; 
-}
 
 // definimos los aux 
 
@@ -35,6 +26,7 @@ bool Octree::contiene(const OctreeNode* nodo ,const  Point3D& p)const {
 
 // obtiene en que cuadrante va el punto segun su posicion relativa al centro
 int Octree::obtenerOctante(const  OctreeNode* nodo,const Point3D& p)const{
+    // calculamos los centors 
     float centroX  = (nodo->minX+nodo->maxX)/ 2.0f ; 
     float centroY = (nodo->minY+nodo->maxY)/2.0f; 
     float centroZ = (nodo->minZ +nodo->maxZ)/ 2.0f ; 
@@ -53,6 +45,92 @@ int Octree::obtenerOctante(const  OctreeNode* nodo,const Point3D& p)const{
     return octante; 
 
 
+}
+
+// divide todo el cuadrante en sus 8 parte o bueno el nodo en sus 8 hijos 
+void Octree::subdividir( OctreeNode*  nodo){
+    float centroX  = (nodo->minX+nodo->maxX)/ 2.0f ; 
+    float centroY = (nodo->minY+nodo->maxY)/2.0f; 
+    float centroZ = (nodo->minZ +nodo->maxZ)/ 2.0f ; 
+
+    // creamos manualmente a los hijos
+
+    nodo->hijos[0] = new OctreeNode(nodo->minX, nodo->minY , nodo->minZ,centroX ,centroY, centroZ, nodo->capacidad ) ;
+    nodo->hijos[1] = new OctreeNode(centroX, nodo->minY, nodo->minZ , nodo->maxX,centroY, centroZ , nodo->capacidad );
+    nodo->hijos[2] = new OctreeNode(nodo->minX ,centroY, nodo->minZ,centroX, nodo->maxY ,centroZ, nodo->capacidad) ;
+    nodo->hijos[3] = new OctreeNode(centroX, centroY ,nodo->minZ,nodo->maxX,nodo->maxY, centroZ ,nodo->capacidad );
+    nodo->hijos[4] = new OctreeNode(nodo->minX,nodo->minY,centroZ,centroX , centroY,nodo->maxZ,nodo->capacidad);
+    nodo->hijos[5] = new OctreeNode(centroX , nodo->minY, centroZ, nodo->maxX,centroY ,nodo->maxZ, nodo->capacidad ) ;
+    nodo->hijos[6] = new OctreeNode(nodo->minX, centroY,centroZ ,centroX, nodo->maxY,nodo->maxZ , nodo->capacidad ) ;
+    nodo->hijos[7] = new OctreeNode(centroX,centroY,centroZ, nodo->maxX , nodo->maxY,nodo->maxZ, nodo->capacidad );
+
+    // redistribuimos los puntos que ya tenia el nodo padre en los hijos
+    for(int i = 0 ;i <(int)nodo->puntos.size() ;i++){ // casteamos el  nodo->puntos.size() para que nos de int  sin warnigs
+        int oct=obtenerOctante(nodo, nodo->puntos[i] ); 
+        nodo->hijos[oct]->puntos.push_back(nodo->puntos[i]); 
+    }
+    nodo->puntos.clear() ; // borrarmo el vectos para no  tener el punto tanto en el padre como en los hijos 
+}
+
+// insercion recursiva en el nodo cuadrante
+bool Octree::insertar(OctreeNode*  nodo,const Point3D& p) {
+    if(!contiene(nodo,p)) {  
+        return false; 
+    }
+
+    if(nodo->hijos[0]==nullptr ){
+        if( (int)nodo->puntos.size()< nodo->capacidad ){
+            nodo->puntos.push_back(p); 
+            return true;
+
+        }else {
+            subdividir(nodo) ; 
+
+        }
+
+    }
+
+    int oct = obtenerOctante(nodo, p); 
+    return insertar(nodo->hijos[oct],p) ; 
+
+}
+
+// busqueda recursiva en el nodo cuadrante
+bool Octree::buscar( const  OctreeNode* nodo,const Point3D& p ) const{
+    if( !contiene(nodo,p) ){  // si nos da false es porque no debe estar en el cuadrante entonces salimos
+        return false; 
+    }
+
+
+    //si es hoja revisamos los puntos guardados
+    if( nodo->hijos[0]==nullptr ){
+        for(int i =0; i< (int)nodo->puntos.size(); i++) {
+            if(nodo->puntos[i]==p){
+                return true; 
+            }
+
+        }
+
+        return false ; 
+
+    }
+    // y seguimos buscando al hijo que corresponde
+    int oct = obtenerOctante(nodo,p); 
+    return buscar( nodo->hijos[oct],p); 
+
+
+}
+
+
+// libera recursivamente toda la memoria de los nodos
+void Octree::liberar(OctreeNode* nodo ) {
+    if(nodo==nullptr ){
+        return; 
+    }
+    for(int i = 0; i <8 ;i++ ) {
+        liberar(nodo->hijos[i]); 
+    }
+    delete nodo ; 
 }
 
 // funciones ya de el octree relaciondo a insercion depuntos y busqueda 
